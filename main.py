74b2f96ebe7a55540133f1ce351fb700618e8e34
User8400 <vano_1@list.ru>
2022-05-05 22:16:13 +0300

    add config, gitignore

--- main.py
+++ main.py
@@ -2,15 +2,16 @@
 
 from email.mime import audio
 import speech_recognition
+from config import lang, pause_threshold
 
 def speech():
     sr = speech_recognition.Recognizer()
-    sr.pause_threshold = 0.5
+    sr.pause_threshold = pause_threshold
 
     with speech_recognition.Microphone() as mic:
         sr.adjust_for_ambient_noise(source=mic, duration=0.5)
         audio = sr.listen(source=mic)
-        query = sr.recognize_google(audio_data=audio, language='ru_Ru').lower()
+        query = sr.recognize_google(audio_data=audio, language=lang).lower()
         return query
 
 def main():
